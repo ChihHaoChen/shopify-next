c013810e3d75ed48c0ff86b920ec06c8c5e63cda
ChihHaoChen <chih-hao.chen2@mail.mcgill.ca>
2021-08-02 02:08:54 +0800

    Delete  prettier package that was accidentally imported.

--- framework/shopify/utils/create-checkout.ts
+++ framework/shopify/utils/create-checkout.ts
@@ -2,7 +2,6 @@ import { ApiFetcher } from "@common/types/api"
 import { SHOPIFY_CHECKOUT_ID_COOKIE, SHOPIFY_CHECKOUT_URL_COOKIE, SHOPIFY_COOKIE_EXPIRE } from "@framework/const"
 import { Checkout, CheckoutCreatePayload, Maybe } from "@framework/schema"
 import Cookies from "js-cookie"
-import { check } from "prettier"
 import { checkoutCreateMutation } from "./mutations"
 
 
